8d59cde2d01285ff18c869de6a9f031fa5d7572e
Arun Chaudhary <boron@Aruns-MacBook-Air-2.local>
2023-12-05 02:44:33 +0530

    made responsive

--- components/heroSection/index.tsx
+++ components/heroSection/index.tsx
@@ -8,8 +8,8 @@ const HeroSection = () => {
     <motion.div
     id='about'
     // motion
-    initial={{ opacity: 0.1, y: 100 }}
- whileInView={{ opacity: 1, y:0 }}
+//     initial={{ opacity: 0.1, y: 100 }}
+//  whileInView={{ opacity: 1, y:0 }}
  viewport={{ once: true}}
 
    transition={{ duration: 0.3 }}
